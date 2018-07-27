c2dcd6e14804c3cb5e878f82fc1fdd10e706a45d
Philipp Zieris <philipp.zieris@aisec.fraunhofer.de>
2018-07-27 10:43:29 +0200

    Search for cross compiler instead of hardcoding it

--- buildscripts/crossbuild-raspberry_pi3-llvm-aarch64-poky-linux.sh
+++ buildscripts/crossbuild-raspberry_pi3-llvm-aarch64-poky-linux.sh
@@ -8,6 +8,18 @@ if [ -z "$JOBS" ]; then
   JOBS=$(nproc)
 fi
 
+[ -z "$CROSS_GCC" ] && CROSS_GCC=$(command -v aarch64-linux-gnu-gcc)
+if ! [ -x "$CROSS_GCC" ]; then
+  echo "GNU C compiler for the ARM64 architecture not found"
+  exit 1
+fi
+
+[ -z "$CROSS_GXX" ] && CROSS_GXX=$(command -v aarch64-linux-gnu-g++)
+if ! [ -x "$CROSS_GXX" ]; then
+  echo "GNU C++ compiler for the ARM64 architecture not found"
+  exit 1
+fi
+
 SCRIPT_PATH="$(cd "$(dirname "$0")" ; pwd -P)"
 WS="$SCRIPT_PATH/../.."
 BUILD="$WS/build-raspberry_pi3-aarch64-poky-linux"
@@ -40,10 +52,10 @@ cd "$OUT_CROSS"
 FLAGS="-march=armv8-a -mcpu=cortex-a53 -mlittle-endian -mabi=lp64"
 [ $? -eq 0 ] && cmake -G Ninja "$SRC" \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc \
+  -DCMAKE_C_COMPILER="$CROSS_GCC" \
   -DCMAKE_C_FLAGS="$FLAGS" \
   -DCMAKE_CROSSCOMPILING=True \
-  -DCMAKE_CXX_COMPILER=/usr/bin/aarch64-linux-gnu-g++ \
+  -DCMAKE_CXX_COMPILER="$CROSS_GXX" \
   -DCMAKE_CXX_FLAGS="$FLAGS" \
   -DCMAKE_INSTALL_PREFIX="$SYSROOT" \
   -DCLANG_TABLEGEN="$OUT_NATIVE/bin/clang-tblgen" \
